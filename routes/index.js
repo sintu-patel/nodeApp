ff5199fc843bf610c86ace28207f7e33bae73fa4
sintu-singh <ssingh172@sapient.com>
2015-12-21 09:52:40 +0530

    Worked with json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var path = require('path')
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var obj;
+	fs.readFile('./data/data.json', 'utf8', function(err, data) {
+		if (err) throw err;
+		obj = JSON.parse(data);
+		res.render('index', obj);
+		// res.json(obj);
+	});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Get Data. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var db = req.db;
+	var collection = db.get('ebookData');
+	console.log(collection);
+	collection.find({"title" : "Hello introduction"}, function(e, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+
 });
 
 module.exports = router;
