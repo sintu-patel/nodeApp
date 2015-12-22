fc67f47621768d193d747b6b580168ade089c1b7
sintu-singh <ssingh172@sapient.com>
2015-12-22 14:36:18 +0530

    test js

---
+++ public/javascripts/javascript.js
@@ -0,0 +1,13 @@
+var menuButton = document.querySelector('#open_menu');
+var lessonList = document.querySelector('#lesson_list');
+var hidden = true;
+menuButton.onclick = function() {
+	if (hidden) {
+		lessonList.style.display = 'block';
+		hidden = false;
+	} else {
+		lessonList.style.display = 'none';
+		hidden = true;
+	}
+
+}
