7be7060bdb9c6937e31989810f7e1fbcccef7d79
sintu-singh <ssingh172@sapient.com>
2015-12-29 12:25:56 +0530

    Update Page App

--- routes/insertdata.js
+++ routes/insertdata.js
@@ -3,22 +3,40 @@ var router = express.Router();
 
 /* Get Data. */
 router.all('/', function(req, res, next) {
+	var queryType = req.param('queryType');
 	var pageTitle = req.param('pageTitle');
 	var img = req.param('img');
 	var content = req.param('content');
 	var pageNo = req.param('pageNo');
 	var db = req.db;
 	var collection = db.get('book');
-	collection.insert({
-		'pageTitle': pageTitle,
-		'img': img,
-		'content': content,
-		'pageNo': pageNo
-	}, function() {
-		collection.count({}, function(e, totalCount) {
-			res.send({ 'STATUS':'ok', 'totalCount': totalCount });
+	pageNo = parseInt(pageNo, 10);
+
+	// Insert Data
+	if (queryType === 'insert') {
+		collection.insert({
+			'pageTitle': pageTitle,
+			'img': img,
+			'content': content,
+			'pageNo': pageNo
+		}, function() {
+			collection.count({}, function(e, totalCount) {
+				res.send({ 'STATUS':'ok', 'totalCount': totalCount });
+			});
 		});
-	});
+	}
+
+	else if (queryType === 'update') {
+		collection.update({ 'pageNo': pageNo }, { $set: {
+			'img': img,
+			'content': content,
+			'pageTitle': pageTitle
+		}}, function() {
+			collection.count({}, function(e, totalCount) {
+				res.send({ 'STATUS':'ok', 'totalCount': totalCount });
+			});
+		});
+	}
 
 });
 
